1fb1e553b3db950efa64990e0733b3bd471e1f1b
pillow12360 <ehdcks1256@naver.com>
2024-09-13 13:51:55 +0900

    #1 feat: Prettier 설정 파일 생성 및 Styled Components 의존성 추가, 테스트 코드 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// 테스트용 스타일링된 컴포넌트 생성
+const Button = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 10px 24px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 12px;
+`;
+
 function App() {
-  return <div className="App">초기 세팅</div>;
+  return (
+    <>
+      <div className="App">초기 세팅</div>
+      <Button>클릭하세요</Button>
+    </>
+  );
 }
 
 export default App;
