91c0d1003f62966c51e54fd3e7d3288a353beb5e
pillow12360 <ehdcks1256@naver.com>
2024-09-13 17:51:19 +0900

    #1 feat: 리뷰 요청 사항 반영 - style.ts로 컴포넌트 스타일 설정 분리 및 임포트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-
-// 테스트용 스타일링된 컴포넌트 생성
-const Button = styled.button`
-  background-color: #4caf50;
-  border: none;
-  color: white;
-  padding: 10px 24px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 12px;
-`;
+import * as S from './styles/style';
 
 function App() {
   return (
     <>
       <div className="App">초기 세팅</div>
-      <Button>클릭하세요</Button>
+      <S.Button>스타일드 컴포넌트 테스트</S.Button>
+      <S.Button primary>기본 버튼</S.Button>
     </>
   );
 }
