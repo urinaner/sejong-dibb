84b3c02bde0391773715fb68eb9a20e4579afaa7
2jin2.1031 <2jin2.1031@gmail.com>
2024-09-25 12:16:49 +0900

    #22 refactor: 불필요한 메서드, import 삭제

--- backend/src/main/java/org/example/backend/admin/domain/mapper/SignInMapper.java
+++ backend/src/main/java/org/example/backend/admin/domain/mapper/SignInMapper.java
@@ -1,16 +1,11 @@
 package org.example.backend.admin.domain.mapper;
 
 import org.example.backend.admin.domain.dto.AdminResDto;
-import org.example.backend.admin.domain.dto.SignInReqDto;
 import org.example.backend.admin.domain.dto.SingInResDto;
 import org.example.backend.admin.domain.dto.TokenDto;
 import org.example.backend.admin.domain.entity.Admin;
-import org.example.backend.professor.domain.dto.professor.ProfessorReqDto;
-import org.example.backend.professor.domain.entity.Professor;
 
 public interface SignInMapper {
-    Admin toEntity(SignInReqDto signInReqDto);
-
     AdminResDto toAdminResDto(Admin admin);
 
     SingInResDto toSingInResDto(TokenDto tokenDto, AdminResDto adminResDto);
