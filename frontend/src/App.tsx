07cd3eeaa7c9a83b69a480a0858e22885bf1385b
Yeeun Park <ye6194@naver.com>
2024-09-29 20:51:12 +0900

    feat: 모달 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './pages/Index/Index';
 import Footer from './components/Footer/Footer';
 import SignIn from './pages/Auth/SignIn';
+import Modal from './components/Modal/Modal';
+import { useState, useEffect } from 'react';
 
 const PageContainer = styled.div`
   display: flex;
@@ -12,6 +14,23 @@ const PageContainer = styled.div`
 `;
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = 'hidden'; // 모달이 열리면 스크롤 막음
+    } else {
+      document.body.style.overflow = 'auto'; // 모달이 닫히면 스크롤 복원
+    }
+
+    // 언마운트 시 스크롤 복원
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isModalOpen]);
+
   return (
     <Router>
       <PageContainer>
@@ -24,6 +43,11 @@ function App() {
         </Routes>
         <Footer />
       </PageContainer>
+      <button onClick={openModal}>모달 버튼</button>
+
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        모달 내용을 입력하세요.
+      </Modal>
     </Router>
   );
 }
