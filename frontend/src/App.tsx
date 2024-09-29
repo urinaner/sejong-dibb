ffda60e1d53593d2308efa7a0f018769c3898487
Yeeun Park <ye6194@naver.com>
2024-09-29 21:01:33 +0900

    chore: App.tsx에서 모달 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './pages/Index/Index';
 import Footer from './components/Footer/Footer';
 import SignIn from './pages/Auth/SignIn';
-import Modal from './components/Modal/Modal';
-import useModal from './components/Modal/useModal';
 
 const PageContainer = styled.div`
   display: flex;
@@ -14,8 +12,6 @@ const PageContainer = styled.div`
 `;
 
 function App() {
-  const { isModalOpen, openModal, closeModal } = useModal(); // 커스텀 훅 사용
-
   return (
     <Router>
       <PageContainer>
@@ -28,11 +24,6 @@ function App() {
         </Routes>
         <Footer />
       </PageContainer>
-      <button onClick={openModal}>모달 버튼</button>
-
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        모달 내용을 입력하세요.
-      </Modal>
     </Router>
   );
 }
