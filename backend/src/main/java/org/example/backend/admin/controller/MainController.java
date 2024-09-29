512f8b4c2a7cf9f39a5254dd3a2d54e023f396f4
2jin2.1031 <2jin2.1031@gmail.com>
2024-09-30 08:59:15 +0900

    #34 feat: test API 생성

--- backend/src/main/java/org/example/backend/admin/controller/MainController.java
+++ backend/src/main/java/org/example/backend/admin/controller/MainController.java
@@ -1,2 +1,18 @@
-package org.example.backend.admin.controller;public class MainController {
+package org.example.backend.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP() {
+
+        return "Main Controller";
+    }
 }
