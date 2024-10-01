38b4a1197d6e9264ce519ef53ee2e2ed3c8ae136
pillow12360 <ehdcks1256@naver.com>
2024-10-01 18:01:08 +0900

    #36 chore: signin 및 signout 으로 변경 (단어 통일)

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -6,8 +6,8 @@ import axios from 'axios';
 interface AuthContextType {
   user: string | null;
   isAuthenticated: boolean;
-  login: (userEmail: string, password: string) => Promise<void>;
-  logout: () => void;
+  signin: (userEmail: string, password: string) => Promise<void>;
+  signout: () => void;
 }
 
 interface Props {
@@ -21,48 +21,30 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
   const [user, setUser] = useState<string | null>(null); // 사용자 정보를 저장
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false); // 인증 상태
 
-  // 로그인 요청 함수
-  async function loginRequest(
-    userEmail: string,
-    password: string,
-  ): Promise<string> {
+  // 로그인 함수: 사용자가 로그인할 때 호출
+  const signin = async (userEmail: string, password: string) => {
     try {
       const response = await axios.post('/api/login', {
         email: userEmail,
-        password: password,
+        password,
       });
 
-      if (response.status === 200) {
-        return response.data.token; // 서버로부터 받은 JWT 토큰
-      } else {
-        throw new Error('로그인 실패: 서버 오류');
-      }
+      // 서버에서 토큰을 받아온다
+      const token = response.data.token;
+
+      // 로그인 성공 시 사용자 정보 업데이트
+      setUser(userEmail);
+      setIsAuthenticated(true);
+
+      // 받은 JWT 토큰을 로컬 스토리지 등에 저장
+      localStorage.setItem('token', token);
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.error('로그인 실패: 서버 오류', error.response?.data);
-        throw new Error('서버 오류로 인해 로그인 실패');
-      } else {
-        console.error('로그인 실패: 네트워크 오류 또는 기타 문제', error);
-        throw new Error('네트워크 오류로 인해 로그인 실패');
-      }
+      console.error('로그인 실패:', error);
     }
-  }
-
-  // 로그인 함수: 사용자가 로그인할 때 호출
-  const login = async (userEmail: string, password: string) => {
-    // 서버에 로그인 요청을 보내고 JWT 토큰을 받아오는 로직
-    const token = await loginRequest(userEmail, password);
-
-    // 로그인 성공 시 사용자 정보 업데이트
-    setUser(userEmail);
-    setIsAuthenticated(true);
-
-    // 받은 JWT 토큰을 로컬 스토리지 등에 저장하여 사용
-    localStorage.setItem('token', token);
   };
 
   // 로그아웃 함수: 사용자가 로그아웃할 때 호출
-  const logout = () => {
+  const signout = () => {
     setUser(null);
     setIsAuthenticated(false);
     localStorage.removeItem('token'); // 토큰 삭제
@@ -72,8 +54,8 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
   const authValue: AuthContextType = {
     user, // 현재 사용자 정보
     isAuthenticated, // 로그인 상태
-    login, // 로그인 함수
-    logout, // 로그아웃 함수
+    signin, // 로그인 함수
+    signout, // 로그아웃 함수
   };
 
   // children 컴포넌트에게 value 값을 제공
