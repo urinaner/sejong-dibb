cef779812c814c6e3d58f0a4fc33b6a83a98690d
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-05 15:17:13 +0900

    #32 feat: swagger 액세스 허용

---
+++ backend/src/main/java/org/example/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package org.example.backend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("CodeArena Swagger")
+                .description("CodeArena 유저 및 인증 , ps, 알림에 관한 REST API")
+                .version("1.0.0");
+    }
+}
