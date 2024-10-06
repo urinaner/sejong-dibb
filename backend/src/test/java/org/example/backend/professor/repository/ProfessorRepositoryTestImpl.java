fa7e2fca649c04a30a7f56329095f29cf9287a10
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-06 12:31:51 +0900

    #31 refactor: 불필요한 testClass 삭제

--- backend/src/test/java/org/example/backend/professor/repository/ProfessorRepositoryTestImpl.java
+++
@@ -1,7 +0,0 @@
-//package org.example.backend.professor.repository;
-//
-//public class ProfessorRepositoryTestImpl extends ProfessorRepositoryTest {
-//    public ProfessorRepositoryTestImpl() {
-//        super(professorMapper);
-//    }
-//}
