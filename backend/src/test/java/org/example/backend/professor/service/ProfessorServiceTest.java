966070e6c3fec1a47c6fafd674b0270b23473f61
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-06 12:53:33 +0900

    #31 test: ProfessorService Test 예외 발생 테스트 코드 추가

--- backend/src/test/java/org/example/backend/professor/service/ProfessorServiceTest.java
+++ backend/src/test/java/org/example/backend/professor/service/ProfessorServiceTest.java
@@ -3,6 +3,8 @@ package org.example.backend.professor.service;
 import org.example.backend.department.repository.DepartmentRepository;
 import org.example.backend.professor.domain.dto.professor.ProfessorReqDto;
 import org.example.backend.professor.domain.dto.professor.ProfessorResDto;
+import org.example.backend.professor.exception.ProfessorException;
+import org.example.backend.professor.exception.ProfessorExceptionType;
 import org.example.backend.professor.repository.ProfessorRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,23 +24,85 @@ class ProfessorServiceTest {
     @Autowired ProfessorService professorService;
     @Autowired ProfessorRepository professorRepository;
     @Autowired DepartmentRepository departmentRepository;
+    ProfessorException professorException;
 
     @Test
     @DisplayName("교수 저장 테스트")
     void saveProfessor() {
         // given: 테스트용 교수 DTO 준비
-        ProfessorReqDto professorReqDto = new ProfessorReqDto();
-        professorReqDto.setName("김영한");
-        professorReqDto.setPhoneN("010-1234-5678");
-        professorReqDto.setEmail("younghan@sejong.ac.kr");
-        professorReqDto.setDepartmentId(1L);
+        ProfessorReqDto dto = new ProfessorReqDto();
+        dto.setName("김영한");
+        dto.setPhoneN("010-1234-5678");
+        dto.setEmail("younghan@sejong.ac.kr");
+        dto.setDepartmentId(1L);
 
         // when: 교수 저장 메소드 호출
-        Long savedProfessorId = professorService.saveProfessor(professorReqDto);
+        Long savedProfessorId = professorService.saveProfessor(dto);
 
         // then: 저장된 교수 정보가 존재하는지 확인
         assertTrue(professorRepository.existsById(savedProfessorId));
     }
+
+    @Test
+    @DisplayName("교수 이름 없이 저장 시 예외 발생 테스트")
+    void saveProfessorWithoutName() {
+        // given: 테스트용 교수 DTO 준비
+        ProfessorReqDto dto = new ProfessorReqDto();
+        dto.setPhoneN("010-1234-5678");
+        dto.setEmail("younghan@sejong.ac.kr");
+        dto.setDepartmentId(1L);
+
+        // when: 교수 저장 시 이름이 없는 경우 예외 발생 확인
+        ProfessorException exception = assertThrows(ProfessorException.class, () -> professorService.saveProfessor(dto));
+        assertEquals(ProfessorExceptionType.REQUIRED_NAME, exception.exceptionType());
+    }
+
+    @Test
+    @DisplayName("교수 핸드폰 번호 중복 저장 시 예외 발생 테스트")
+    void saveProfessorWithDuplicatePhone() {
+        // given: 테스트용 교수 DTO 준비
+        ProfessorReqDto dtoSrc = new ProfessorReqDto();
+        dtoSrc.setName("김영한");
+        dtoSrc.setPhoneN("010-1234-5678");
+        dtoSrc.setEmail("younghan@sejong.ac.kr");
+        dtoSrc.setDepartmentId(1L);
+
+        professorService.saveProfessor(dtoSrc);
+
+        ProfessorReqDto dtoDest = new ProfessorReqDto();
+        dtoDest.setName("김세종");
+        dtoDest.setPhoneN("010-1234-5678");
+        dtoDest.setEmail("example@sejong.ac.kr");
+        dtoDest.setDepartmentId(1L);
+
+        // when: 교수 저장 시 핸드폰번호 중복인 경우 예외 발생 확인
+        ProfessorException exception = assertThrows(ProfessorException.class, () -> professorService.saveProfessor(dtoDest));
+        assertEquals(ProfessorExceptionType.DUPLICATE_PHONE, exception.exceptionType());
+    }
+
+    @Test
+    @DisplayName("교수 이메일 중복 저장 시 예외 발생 테스트")
+    void saveProfessorWithDuplicateEmail() {
+        // given: 테스트용 교수 DTO 준비
+        ProfessorReqDto dtoSrc = new ProfessorReqDto();
+        dtoSrc.setName("김영한");
+        dtoSrc.setPhoneN("010-1234-5678");
+        dtoSrc.setEmail("younghan@sejong.ac.kr");
+        dtoSrc.setDepartmentId(1L);
+
+        professorService.saveProfessor(dtoSrc);
+
+        ProfessorReqDto dtoDest = new ProfessorReqDto();
+        dtoDest.setName("김세종");
+        dtoDest.setPhoneN("010-3445-5678");
+        dtoDest.setEmail("younghan@sejong.ac.kr");
+        dtoDest.setDepartmentId(1L);
+
+        // when: 교수 저장 시 핸드폰번호 중복인 경우 예외 발생 확인
+        ProfessorException exception = assertThrows(ProfessorException.class, () -> professorService.saveProfessor(dtoDest));
+        assertEquals(ProfessorExceptionType.DUPLICATE_EMAIL, exception.exceptionType());
+    }
+
 
     @Test
     @DisplayName("교수 조회 테스트")
@@ -58,6 +122,23 @@ class ProfessorServiceTest {
         // then: 조회된 교수 정보가 null이 아니고, ID가 일치하는지 확인
         assertNotNull(professorResDto);
         assertEquals(savedProfessorId, professorResDto.getId());
+    }
+
+    @Test
+    @DisplayName("교수 id 조회 시 없는 교수인 경우 예외 발생 테스트")
+    void findProfessorByIdWithNotExistId() {
+        // given: 먼저 교수를 저장하여 데이터 준비
+        ProfessorReqDto professorReqDto = new ProfessorReqDto();
+        professorReqDto.setName("김영한");
+        professorReqDto.setPhoneN("010-1234-5678");
+        professorReqDto.setEmail("younghan@sejong.ac.kr");
+        professorReqDto.setDepartmentId(1L); // 가상의 부서 ID
+
+        Long savedProfessorId = professorService.saveProfessor(professorReqDto);
+
+        // when: 존재하지 않는 교수 ID로 교수 조회 시도
+        ProfessorException exception = assertThrows(ProfessorException.class, () -> professorService.getProfessor(savedProfessorId + 1));
+        assertEquals(ProfessorExceptionType.NOT_FOUND_PROFESSOR, exception.exceptionType());
     }
 
     @Test
@@ -84,6 +165,7 @@ class ProfessorServiceTest {
         // then: 수정된 교수 정보가 제대로 저장되었는지 확인
         assertEquals("김수정", updatedProfessor.getName());
         assertEquals("010-8765-4321", updatedProfessor.getPhoneN());
+        assertEquals("sujeong@sejong.ac.kr", updatedProfessor.getEmail());
     }
 
     @Test
