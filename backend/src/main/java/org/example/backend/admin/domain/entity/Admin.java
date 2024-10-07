f56dbae5d73b6f6853af4151bd7c582c3092afad
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-07 17:54:03 +0900

    #63 feat: admin 필드 속성 수정

--- backend/src/main/java/org/example/backend/admin/domain/entity/Admin.java
+++ backend/src/main/java/org/example/backend/admin/domain/entity/Admin.java
@@ -15,25 +15,23 @@ import java.util.List;
 @Table(name = "admin")
 public class Admin {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
-    @Column(name = "login_id")
+    @Column(name = "login_id", nullable = false)
     private String loginId;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "username")
+    @Column(name = "username", nullable = false)
     private String username;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "role")
+    @Column(name = "role", nullable = false)
     private String role;
 
     @OneToMany()
