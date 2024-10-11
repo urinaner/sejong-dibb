cb06a71c4bedb547d25dd3f6a6197835d22d9cc4
2jin2.1031 <2jin2.1031@gmail.com>
2024-10-11 22:45:22 +0900

    #70 feat: login 주소 swagger에 추가

--- backend/src/main/java/org/example/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/org/example/backend/global/config/SwaggerConfig.java
@@ -2,12 +2,16 @@ package org.example.backend.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.media.StringSchema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
+@Configuration // 스프링 실행시 설정파일 읽어들이기 위한 어노테이션
 @EnableWebMvc
 public class SwaggerConfig {
 
@@ -15,6 +19,11 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
+                .path("/api/admin/login", new PathItem().post(new Operation()
+                        .summary("Admin login")
+                        .description("로그인 기능은 Spring Security 기본 로그인 기능을 따르며, form-data 형식으로 로그인 ID와 비밀번호를 전송합니다.")
+                        .addParametersItem(new Parameter().name("loginId").in("query").required(true).description("로그인 ID").schema(new StringSchema()))
+                        .addParametersItem(new Parameter().name("password").in("query").required(true).description("비밀번호").schema(new StringSchema()))))
                 .info(apiInfo());
     }
 
