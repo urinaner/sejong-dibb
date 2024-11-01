aa8b910163f4dea5e2d10c657f012085fc903e54
장영재 <urinaner@naver.com>
2024-11-01 14:55:46 +0900

    test: 서비스, 레포지토리 통합

---
+++ backend/src/test/java/org/example/backend/IntegrationTestSupport.java
@@ -0,0 +1,20 @@
+package org.example.backend;
+
+import org.example.backend.department.domain.mapper.DepartmentMapper;
+import org.example.backend.professor.domain.mapper.ProfessorMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+
+    @Autowired
+    protected DepartmentMapper departmentMapper;
+
+    @Autowired
+    protected ProfessorMapper professorMapper;
+
+}
