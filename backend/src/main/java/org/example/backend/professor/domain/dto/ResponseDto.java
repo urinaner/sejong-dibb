88967317f8ff01197c4fb7a289d49f906dc38181
장영재 <urinaner@naver.com>
2024-11-02 16:19:13 +0900

    feat: 페이징 Dto

--- backend/src/main/java/org/example/backend/professor/domain/dto/ResponseDto.java
+++ backend/src/main/java/org/example/backend/professor/domain/dto/ResponseDto.java
@@ -2,20 +2,24 @@ package org.example.backend.professor.domain.dto;
 
 import lombok.*;
 
-@Data
-@NoArgsConstructor
-@Builder
-public class ResponseDto {
-
-    private String response;
-
+@Getter
+public class ResponseDto<T> {
     private String message;
+    private int page;
+    private int totalPage;
+    private T data;
 
-    private Object data;
-
-    public ResponseDto(String response, String message, Object data) {
-        this.response = response;
+    public ResponseDto(String message, int page, int totalPage, T data) {
         this.message = message;
+        this.page = page;
+        this.totalPage = totalPage;
         this.data = data;
+    }
+    public static <T> ResponseDto<T> of(String message, int page, int totalPage, T data) {
+        return new ResponseDto<>(message, page, totalPage, data);
+    }
+
+    public static <T> ResponseDto<T> ok(int page, int totalPage, T data) {
+        return of("조회성공", page, totalPage, data);
     }
 }
