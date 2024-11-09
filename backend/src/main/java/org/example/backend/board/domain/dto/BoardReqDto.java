4aad66f035212ec59b0eb1f78b701d8ac9948e05
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-09 20:58:21 +0900

    feat: 게시판 API 요청 필드 및 예시값 수정

--- backend/src/main/java/org/example/backend/board/domain/dto/BoardReqDto.java
+++ backend/src/main/java/org/example/backend/board/domain/dto/BoardReqDto.java
@@ -1,7 +1,6 @@
 package org.example.backend.board.domain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,18 +14,12 @@ public class BoardReqDto {
     @Schema(description = "게시판 내용", example = "이것은 게시판 내용입니다.", required = true)
     private String content;
 
-    @Schema(description = "게시판 작성자", example = "홍길동")
-    private String writer;
-
     @Schema(description = "첨부 파일", example = "file.pdf")
     private String file;
 
-    @Schema(description = "게시판 생성일", example = "2024-11-09")
-    private String createDate;
-
-    @Schema(description = "게시판 카테고리", example = "일반")
+    @Schema(description = "게시판 카테고리", example = "graduate")
     private String category;
 
-    @Schema(description = "연관된 학과 ID", example = "123", required = true)
+    @Schema(description = "연관된 학과 ID", example = "1", required = true)
     private Long departmentId;
 }
