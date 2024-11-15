9a70a5fd81963b9b7b70b88724f995620e6de188
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-15 13:58:50 +0900

    feat: 요청 본문 비어 있는 경우 예외 처리 추가

--- backend/src/main/java/org/example/backend/common/exception/ExceptionControllerAdvice.java
+++ backend/src/main/java/org/example/backend/common/exception/ExceptionControllerAdvice.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -38,6 +39,13 @@ public class ExceptionControllerAdvice {
         log.error("예상하지 못한 예외가 발생했습니다. URI: {}, ", request.getRequestURI(), e);
         return ResponseEntity.internalServerError()
                 .body(new ExceptionResponse("알 수 없는 오류가 발생했습니다."));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpServletRequest request, HttpMessageNotReadableException e) {
+        log.info("요청 본문이 비어 있습니다. URI: {}", request.getRequestURI());
+        return ResponseEntity.status(BAD_REQUEST)
+                .body(new ExceptionResponse("요청 본문이 비어 있습니다."));
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
