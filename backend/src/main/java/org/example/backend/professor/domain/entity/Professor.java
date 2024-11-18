51e305350becc159c20894b2c16a37d8ed67b057
장영재 <urinaner@naver.com>
2024-11-18 18:54:19 +0900

    refactor: Professor 빌더 패턴, 정적 펙토리 적용

--- backend/src/main/java/org/example/backend/professor/domain/entity/Professor.java
+++ backend/src/main/java/org/example/backend/professor/domain/entity/Professor.java
@@ -3,14 +3,16 @@ package org.example.backend.professor.domain.entity;
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.backend.department.domain.entity.Department;
+import org.example.backend.professor.domain.dto.professor.ProfessorReqDto;
 import org.example.backend.thesis.domain.entity.Thesis;
 
 @Entity
-@Data
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "professor")
 public class Professor {
 
@@ -43,10 +45,43 @@ public class Professor {
     @Column(name = "profileImage")
     private String profileImage;
 
-    @ManyToOne
-    @JoinColumn(name = "department_id")
-    private Department department;
-
     @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Thesis> theses = new ArrayList<>();
+
+    @Builder
+    private Professor(String name, String major, String phoneN, String email,
+                      String position, String homepage, String lab, String profileImage) {
+        this.name = name;
+        this.major = major;
+        this.phoneN = phoneN;
+        this.email = email;
+        this.position = position;
+        this.homepage = homepage;
+        this.lab = lab;
+        this.profileImage = profileImage;
+    }
+
+    public static Professor of(ProfessorReqDto dto) {
+        return Professor.builder()
+                .name(dto.getName())
+                .major(dto.getMajor())
+                .phoneN(dto.getPhoneN())
+                .email(dto.getEmail())
+                .position(dto.getPosition())
+                .homepage(dto.getHomepage())
+                .lab(dto.getLab())
+                .profileImage(dto.getProfileImage())
+                .build();
+    }
+
+    public void update(ProfessorReqDto dto) {
+        this.name = dto.getName();
+        this.major = dto.getMajor();
+        this.phoneN = dto.getPhoneN();
+        this.email = dto.getEmail();
+        this.position = dto.getPosition();
+        this.homepage = dto.getHomepage();
+        this.lab = dto.getLab();
+        this.profileImage = dto.getProfileImage();
+    }
 }
