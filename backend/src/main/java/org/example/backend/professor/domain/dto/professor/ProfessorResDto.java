371a5b55871114d2f9a4dccb924fddc0a23bcc51
장영재 <urinaner@naver.com>
2024-11-18 18:54:40 +0900

    refactor: ProfessorDto 빌더 패턴, 정적 펙토리 적용

--- backend/src/main/java/org/example/backend/professor/domain/dto/professor/ProfessorResDto.java
+++ backend/src/main/java/org/example/backend/professor/domain/dto/professor/ProfessorResDto.java
@@ -1,9 +1,14 @@
 package org.example.backend.professor.domain.dto.professor;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.professor.domain.entity.Professor;
 
-@Data
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfessorResDto {
     @Schema(description = "교수 고유 아이디", example = "54321")
     private Long id;
@@ -31,4 +36,32 @@ public class ProfessorResDto {
 
     @Schema(description = "교수 프로필 이미지", example = "https://www.sju.ac.kr/professor/example/profile.jpg")
     private String profileImage;
+
+    @Builder
+    private ProfessorResDto(Long id, String name, String major, String phoneN, String email,
+                            String position, String homepage, String lab, String profileImage) {
+        this.id = id;
+        this.name = name;
+        this.major = major;
+        this.phoneN = phoneN;
+        this.email = email;
+        this.position = position;
+        this.homepage = homepage;
+        this.lab = lab;
+        this.profileImage = profileImage;
+    }
+
+    public static ProfessorResDto of(Professor professor) {
+        return ProfessorResDto.builder()
+                .id(professor.getId())
+                .name(professor.getName())
+                .major(professor.getMajor())
+                .phoneN(professor.getPhoneN())
+                .email(professor.getEmail())
+                .position(professor.getPosition())
+                .homepage(professor.getHomepage())
+                .lab(professor.getLab())
+                .profileImage(professor.getProfileImage())
+                .build();
+    }
 }
