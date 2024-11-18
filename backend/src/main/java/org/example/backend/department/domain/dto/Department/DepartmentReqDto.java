853d55933e4125afa66ca1bd0f7420a45b310f2c
장영재 <urinaner@naver.com>
2024-11-18 18:39:12 +0900

    feat: DepartmentDto 빌더 패턴, 정적 펙토리 적용

--- backend/src/main/java/org/example/backend/department/domain/dto/Department/DepartmentReqDto.java
+++ backend/src/main/java/org/example/backend/department/domain/dto/Department/DepartmentReqDto.java
@@ -1,10 +1,12 @@
 package org.example.backend.department.domain.dto.Department;
 
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DepartmentReqDto {
     private String koreanName;
     private String englishName;
@@ -15,4 +17,30 @@ public class DepartmentReqDto {
     private String workHour;
     private String map;
 
+    @Builder
+    private DepartmentReqDto(String koreanName, String englishName, String intro, String phoneN,
+                             String location, String educationalObjective, String workHour, String map) {
+        this.koreanName = koreanName;
+        this.englishName = englishName;
+        this.intro = intro;
+        this.phoneN = phoneN;
+        this.location = location;
+        this.educationalObjective = educationalObjective;
+        this.workHour = workHour;
+        this.map = map;
+    }
+
+    public static DepartmentReqDto of(String koreanName, String englishName, String intro, String phoneN,
+                                      String location, String educationalObjective, String workHour, String map) {
+        return DepartmentReqDto.builder()
+                .koreanName(koreanName)
+                .englishName(englishName)
+                .intro(intro)
+                .phoneN(phoneN)
+                .location(location)
+                .educationalObjective(educationalObjective)
+                .workHour(workHour)
+                .map(map)
+                .build();
+    }
 }
