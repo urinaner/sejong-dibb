af4f24fd2a96cdbb3aff20aeef36c017ba48f52a
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-22 20:40:56 +0900

    feat: User 엔티티 기본 필드 구현

---
+++ backend/src/main/java/org/example/backend/user/domain/entity/User.java
@@ -0,0 +1,32 @@
+package org.example.backend.user.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "major")
+    private String major;
+
+    @Column(name = "phone", unique = true)
+    private String phoneN;
+}
