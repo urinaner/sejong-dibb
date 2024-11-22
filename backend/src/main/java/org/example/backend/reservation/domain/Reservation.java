7d065d3b782cb56b9b075d141244964818e28926
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-22 20:46:24 +0900

    feat: Reservation 엔티티 기본 필드 구현

---
+++ backend/src/main/java/org/example/backend/reservation/domain/Reservation.java
@@ -0,0 +1,39 @@
+package org.example.backend.reservation.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @Column(name = "purpose")
+    private ReservationPurpose purpose;
+
+    @Column(name = "etc")
+    private String etc;
+
+    @Column(name = "status")
+    private ReservationStatus status;
+}
---
+++ backend/src/main/java/org/example/backend/reservation/domain/ReservationPurpose.java
@@ -0,0 +1,4 @@
+package org.example.backend.reservation.domain;
+
+public enum ReservationPurpose {
+}
