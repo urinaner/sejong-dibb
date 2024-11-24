906813a5757f7845f85a06886d8b2640d0a4f781
장영재 <urinaner@naver.com>
2024-11-24 18:50:54 +0900

    refactor: ReservationService 분리

--- backend/src/main/java/org/example/backend/reservation/service/ReservationService.java
+++ backend/src/main/java/org/example/backend/reservation/service/ReservationService.java
@@ -3,9 +3,9 @@ package org.example.backend.reservation.service;
 import static org.example.backend.reservation.exception.ReservationExceptionType.*;
 import static org.example.backend.seminarRoom.exception.SeminarRoomExceptionType.NOT_FOUND_SEMINAR_ROOM;
 
-import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.reservation.exception.ReservationException;
+import org.example.backend.reservation.service.validation.ReservationValidator;
 import org.example.backend.seminarRoom.domain.SeminarRoom;
 import org.example.backend.seminarRoom.exception.SeminarRoomException;
 import org.example.backend.seminarRoom.repository.SeminarRoomRepository;
@@ -14,12 +14,8 @@ import org.example.backend.reservation.domain.ReservationStatus;
 import org.example.backend.reservation.domain.dto.ReservationReqDto;
 import org.example.backend.reservation.domain.dto.ReservationResDto;
 import org.example.backend.reservation.repository.ReservationRepository;
-import org.example.backend.timetable.domain.WeekDay;
-import org.example.backend.timetable.repository.TimetableRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,7 +25,7 @@ import java.util.stream.Collectors;
 public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final SeminarRoomRepository seminarRoomRepository;
-    private final TimetableRepository timetableRepository;
+    private final ReservationValidator validator;
 
 
     @Transactional
@@ -37,9 +33,7 @@ public class ReservationService {
         SeminarRoom seminarRoom = seminarRoomRepository.findById(reqDto.getSeminarRoomId())
                 .orElseThrow(() -> new SeminarRoomException(NOT_FOUND_SEMINAR_ROOM));
 
-        validateReservationTime(reqDto);
-        validateTimeTableOverlap(reqDto, seminarRoom.getId());
-        validateReservationOverlap(reqDto, seminarRoom.getId());
+        validator.validate(reqDto);
 
         Reservation reservation = Reservation.of(reqDto, seminarRoom);
         reservationRepository.save(reservation);
@@ -59,20 +53,6 @@ public class ReservationService {
         return ReservationResDto.of(reservation);
     }
 
-    @Transactional
-    public ReservationResDto updateReservation(Long id, ReservationReqDto reqDto) {
-        Reservation reservation = reservationRepository.findById(id)
-                .orElseThrow(() -> new ReservationException(NOT_FOUND_RESERVATION));
-
-        SeminarRoom seminarRoom = seminarRoomRepository.findById(reqDto.getSeminarRoomId())
-                .orElseThrow(() -> new SeminarRoomException(NOT_FOUND_SEMINAR_ROOM));
-
-        validateReservationTime(reqDto);
-        validateReservationOverlap(reqDto, seminarRoom.getId());
-
-        reservation.update(reqDto, seminarRoom);
-        return ReservationResDto.of(reservation);
-    }
 
     @Transactional
     public ReservationResDto updateReservationStatus(Long id, ReservationStatus status) {
@@ -88,51 +68,5 @@ public class ReservationService {
         Reservation reservation = reservationRepository.findById(id)
                 .orElseThrow(() -> new ReservationException(NOT_FOUND_RESERVATION));
         reservationRepository.delete(reservation);
-    }
-
-    private void validateTimeTableOverlap(ReservationReqDto reqDto, Long seminarRoomId) {
-        // DayOfWeek를 WeekDay로 변환
-        WeekDay weekDay = WeekDay.from(reqDto.getStartTime().getDayOfWeek());
-
-        boolean hasTimeTableConflict = timetableRepository.existsByTimeOverlap(
-                seminarRoomId,
-                reqDto.getStartTime().toLocalDate(),
-                reqDto.getStartTime().toLocalTime(),
-                reqDto.getEndTime().toLocalTime(),
-                weekDay
-        );
-
-        if (hasTimeTableConflict) {
-            throw new ReservationException(CONFLICT_TIMETABLE);
-        }
-    }
-
-    private void validateReservationOverlap(ReservationReqDto reqDto, Long seminarRoomId) {
-        boolean hasReservationConflict = reservationRepository.existsByTimePeriod(
-                seminarRoomId,
-                reqDto.getStartTime(),
-                reqDto.getEndTime(),
-                ReservationStatus.APPROVED
-        );
-
-        if (hasReservationConflict) {
-            throw new ReservationException(EXIST_ALREADY_RESERVATION);
-        }
-    }
-
-    private void validateReservationTime(ReservationReqDto reqDto) {
-        LocalDateTime now = LocalDateTime.now();
-//        if (reqDto.getStartTime().isBefore(now)) {
-//            throw new ReservationException(PAST_TIME_RESERVATION);
-//        }
-        if (reqDto.getStartTime().isAfter(reqDto.getEndTime())) {
-            throw new ReservationException(INVALID_TIME_ORDER);
-        }
-
-        // 예약 시간이 너무 긴 경우 체크 (예: 2시간 초과)
-        long hours = ChronoUnit.HOURS.between(reqDto.getStartTime(), reqDto.getEndTime());
-        if (hours > 2) {
-            throw new ReservationException(EXCEEDS_MAX_DURATION);
-        }
     }
 }
