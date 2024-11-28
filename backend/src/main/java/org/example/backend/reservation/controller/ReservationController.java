cddccf645d0ebb0acee9064c68ab8d4d8c0afb93
2jin2.1031 <2jin2.1031@gmail.com>
2024-11-28 17:33:17 +0900

    refactor: 줄바꿈 정리

--- backend/src/main/java/org/example/backend/reservation/controller/ReservationController.java
+++ backend/src/main/java/org/example/backend/reservation/controller/ReservationController.java
@@ -1,7 +1,7 @@
 package org.example.backend.reservation.controller;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.reservation.domain.ReservationStatus;
 import org.example.backend.reservation.domain.dto.ReservationReqDto;
@@ -9,9 +9,15 @@ import org.example.backend.reservation.domain.dto.ReservationResDto;
 import org.example.backend.reservation.service.ReservationService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,11 +40,13 @@ public class ReservationController {
     }
 
     @GetMapping("seminarRoom/{seminarRoomId}")
-    public ResponseEntity<List<ReservationResDto>> getReservationsBySeminarRoomAndDate(@PathVariable(name = "seminarRoomId") Long seminarRoomId,
-                                                                                       @RequestParam(value = "status") ReservationStatus status,
-                                                                                       @RequestParam(value = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
-                                                                                       ) {
-        List<ReservationResDto> reservations = reservationService.getReservationsByRoomAndDate(seminarRoomId, date, status);
+    public ResponseEntity<List<ReservationResDto>> getReservationsBySeminarRoomAndDate(
+            @PathVariable(name = "seminarRoomId") Long seminarRoomId,
+            @RequestParam(value = "status") ReservationStatus status,
+            @RequestParam(value = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        List<ReservationResDto> reservations = reservationService.getReservationsByRoomAndDate(seminarRoomId, date,
+                status);
         return ResponseEntity.ok(reservations);
     }
 
