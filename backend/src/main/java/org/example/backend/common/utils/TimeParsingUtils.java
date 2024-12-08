e64d25e6a6cacc7d563c1c4c37203d3e4aae6dbb
장영재 <urinaner@naver.com>
2024-12-08 14:28:38 +0900

    refactor: 사용자가 확인하기 쉬운 형태로 포맷

--- backend/src/main/java/org/example/backend/common/utils/TimeParsingUtils.java
+++ backend/src/main/java/org/example/backend/common/utils/TimeParsingUtils.java
@@ -2,8 +2,14 @@ package org.example.backend.common.utils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class TimeParsingUtils {
+    private static final int SEC = 60;
+    private static final int MIN = 60;
+    private static final int HOUR = 24;
+    private static final int DAY = 30;
+    private static final int MONTH = 12;
 
     public static LocalDateTime formatterLocalDateTime(String time) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
@@ -13,5 +19,33 @@ public class TimeParsingUtils {
     public static String formatterString(LocalDateTime time) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         return time.format(formatter);
+    }
+
+    public static String toRelativeTimeFormat(LocalDateTime localDateTime) {
+        LocalDateTime now = LocalDateTime.now();
+        long diffTime = localDateTime.until(now, ChronoUnit.SECONDS);
+
+        if (diffTime < SEC) {
+            return diffTime + "초 전";
+        }
+        diffTime = diffTime / SEC;
+        if (diffTime < MIN) {
+            return diffTime + "분 전";
+        }
+        diffTime = diffTime / MIN;
+        if (diffTime < HOUR) {
+            return diffTime + "시간 전";
+        }
+        diffTime = diffTime / HOUR;
+        if (diffTime < DAY) {
+            return diffTime + "일 전";
+        }
+        diffTime = diffTime / DAY;
+        if (diffTime < MONTH) {
+            return diffTime + "개월 전";
+        }
+
+        diffTime = diffTime / MONTH;
+        return diffTime + "년 전";
     }
 }
