16d2a91edfd18f55fbcb7bd1e0ebbce320e749ed
pillow12360 <ehdcks1256@naver.com>
2024-12-28 14:51:19 +0900

    #219 style: 컬러 일괄관리 수정

--- frontend/src/constants/colors.ts
+++ frontend/src/constants/colors.ts
@@ -1,60 +1,10 @@
 export const SEJONG_COLORS = {
-  CRIMSON_RED: {
-    name: 'Sejong Crimson Red',
-    code: 'PANTONE 200c',
-    value: 'C 15.78, M 100, Y 77.25, K 6.31',
-    rgb: 'R 195, G 0, B 47',
-  },
-  GRAY: {
-    name: 'Sejong Gray',
-    code: 'PANTONE 7545C',
-    value: 'C55, M25, Y10, K65',
-    rgb: 'R81, G98, B111',
-  },
-  COOL_GRAY: {
-    name: 'Sejong Cool Gray',
-    code: 'PANTONE Cool Gray 2C',
-    value: 'C0, M0, Y5, K7',
-    rgb: 'R213, G214, B210',
-  },
-  WARM_GRAY1: {
-    name: 'Sejong Warm Gray1',
-    code: 'PANTONE Warm Gray 4C',
-    value: 'C0, M5, Y15, K25',
-    rgb: 'R183, G177, B169',
-  },
-  WARM_GRAY2: {
-    name: 'Sejong Warm Gray2',
-    code: 'PANTONE Warm Gray 11c',
-    value: 'C0, M17, Y34, K62',
-    rgb: 'R131, G114, B89',
-  },
-  LIGHT_GRAY: {
-    name: 'Sejong Light Gray',
-    code: 'PANTONE 7544C',
-    value: 'C33, M14, Y11, K31',
-    rgb: 'R137, G150, B160',
-  },
-  IVORY: {
-    name: 'Sejong Ivory',
-    code: 'PANTONE 9060C',
-    value: 'C2, M1, Y10, K0',
-    rgb: 'R255, G253, B228',
-  },
-  YELLOW: {
-    name: 'Sejong Yellow',
-    code: 'PANTONE 134C',
-    value: 'C0, M9, Y45, K0',
-    rgb: 'R255, G234, B155',
-  },
-  GOLD: {
-    name: 'Sejong Gold',
-    code: 'PANTONE 876C',
-    rgb: 'Special Gold Color',
-  },
-  SILVER: {
-    name: 'Sejong Silver',
-    code: 'PANTONE 877C',
-    rgb: 'Special Silver Color',
-  },
+  CRIMSON_RED: '#C3002F',
+  GRAY: '#51626F',
+  COOL_GRAY: '#D5D6D2',
+  WARM_GRAY1: '#B7B1A9',
+  WARM_GRAY2: '#837259',
+  LIGHT_GRAY: '#8996A0',
+  IVORY: '#FFFDE4',
+  YELLOW: '#FFE99B',
 } as const;
