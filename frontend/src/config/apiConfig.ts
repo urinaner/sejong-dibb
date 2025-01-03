c0dce42d5d591728ee51940f4567690ded7977bf
pillow12360 <ehdcks1256@naver.com>
2025-01-03 22:40:54 +0900

    #243 fix: 전역 axios 설정 - 쿠키 전송 설정

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,4 +1,9 @@
+import axios from 'axios';
+
 const API_URL = process.env.REACT_APP_API_URL;
+
+axios.defaults.withCredentials = true;  // 모든 요청에 대해 크리덴셜 전송
+axios.defaults.baseURL = API_URL;       // 기본 URL 설정
 
 export interface BoardReqDto {
   title: string;
