7cae21b6f9dddfb96dbf19c60d95e33a4ed9c274
2jin2.1031 <2jin2.1031@gmail.com>
2025-01-03 15:46:11 +0900

    feat: 뉴스 엔티티 설계

---
+++ backend/src/main/java/org/example/backend/news/domain/entity/News.java
@@ -0,0 +1,67 @@
+package org.example.backend.news.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.news.domain.dto.NewsReqDto;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "create_date")
+    private String createDate;
+
+    @Column(name = "link")
+    private String link;
+
+    @Column(name = "image")
+    private String image;
+
+
+    @Builder
+    private News(String title, String content, String createDate, String link,
+                 String image) {
+        this.title = title;
+        this.content = content;
+        this.createDate = createDate;
+        this.link = link;
+        this.image = image;
+    }
+
+    public static News of(NewsReqDto dto) {
+        return News.builder()
+                .title(dto.getTitle())
+                .content(dto.getWriter())
+                .createDate(dto.getPlace())
+                .link(dto.getStartDate())
+                .image(dto.getEndDate())
+                .build();
+    }
+
+    public void update(NewsReqDto dto) {
+        this.title = dto.getTitle();
+        this.content = dto.getWriter();
+        this.createDate = dto.getPlace();
+        this.link = dto.getStartDate();
+        this.image = dto.getEndDate();
+    }
+}
