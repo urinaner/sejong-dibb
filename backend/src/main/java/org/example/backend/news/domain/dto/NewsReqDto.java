09d734669bf74475125a052099fbeb0ac30eaa36
2jin2.1031 <2jin2.1031@gmail.com>
2025-01-08 21:36:34 +0900

    fix: 뉴스 reqDto에서 생성일 필드 삭제

--- backend/src/main/java/org/example/backend/news/domain/dto/NewsReqDto.java
+++ backend/src/main/java/org/example/backend/news/domain/dto/NewsReqDto.java
@@ -18,27 +18,23 @@ public class NewsReqDto {
     @NotBlank(message = "내용은 필수 입력값입니다.")
     @Size(max = 5000, message = "내용은 최대 5000자까지 입력 가능합니다.")
     private String content;
-
-    private String createDate;
     private String link;
     private String image;
 
     @Builder
-    private NewsReqDto(String title, String content, String createDate,
+    private NewsReqDto(String title, String content,
                        String link, String image) {
         this.title = title;
         this.content = content;
-        this.createDate = createDate;
         this.link = link;
         this.image = image;
     }
 
-    public static NewsReqDto of(String title, String content, String createDate,
+    public static NewsReqDto of(String title, String content,
                                 String link, String image) {
         return NewsReqDto.builder()
                 .title(title)
                 .content(content)
-                .createDate(createDate)
                 .link(link)
                 .image(image)
                 .build();
