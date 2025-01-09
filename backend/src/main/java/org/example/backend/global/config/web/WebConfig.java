bc8b7907021c54252cd66f8d23c6a128e2a08aa4
장영재 <urinaner@naver.com>
2025-01-09 21:13:20 +0900

    fix : 불필요 로직 제거

--- backend/src/main/java/org/example/backend/global/config/web/WebConfig.java
+++ backend/src/main/java/org/example/backend/global/config/web/WebConfig.java
@@ -1,6 +1,5 @@
 package org.example.backend.global.config.web;
 
-import org.example.backend.global.aop.AuthUserResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,18 +9,6 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    private final AuthUserResolver authUserResolver;
-
-    public WebConfig(AuthUserResolver authUserResolver) {
-        this.authUserResolver = authUserResolver;
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(authUserResolver);
-    }
-
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
