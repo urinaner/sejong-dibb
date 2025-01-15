a84f4aaddf3a1c88827876f25ae031036ea66c3a
2jin2.1031 <2jin2.1031@gmail.com>
2025-01-16 01:19:57 +0900

    refactor: loginfilter 내 불필요한 jwt 코드 삭제

--- backend/src/main/java/org/example/backend/jwt/LoginFilter.java
+++ backend/src/main/java/org/example/backend/jwt/LoginFilter.java
@@ -39,30 +39,11 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         log.info("LoginFilter.attemptAuthentication");
 
-        // 클라이언트가 보낸 username과 password를 추출
+        // 클라이언트가 보낸 loginId와 password를 추출
         String loginId = obtainUsername(request);
         String password = obtainPassword(request);
+
         log.info("loginId: " + loginId);
-
-        // 요청 헤더에서 JWT가 존재하는지 확인
-        String authorization = request.getHeader("Authorization");
-
-        // JWT 토큰이 존재하면 (즉, 로그인을 이미 한 상태에서 다시 로그인 요청이 들어왔을 때)
-        if (authorization != null && authorization.startsWith("Bearer ")) {
-            // JWT가 존재하는 경우, JWT에서 username 추출
-            String token = authorization.split(" ")[1];
-            String tokenLoginId = jwtUtil.getLoginId(token);
-
-            log.info("tokenLoginId: " + tokenLoginId);
-
-            // 클라이언트가 보낸 username과 JWT의 username이 일치하는지 확인
-            if (!loginId.equals(tokenLoginId)) {
-                log.error("LoginId in the form does not match the loginId in the token");
-                throw new AuthenticationException("loginId mismatch") {};
-            }
-        } else {
-            log.info("No Authorization header found. Proceeding with standard login.");
-        }
 
         // AuthenticationManager를 통해 loginId와 password로 인증 시도
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginId, password, null);
