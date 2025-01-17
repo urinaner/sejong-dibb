782206e91c3af3b5e853ad2979060dc9d29f534e
pillow12360 <ehdcks1256@naver.com>
2025-01-17 12:01:10 +0900

    #275 fix: axios 쿠키허용 전역 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
