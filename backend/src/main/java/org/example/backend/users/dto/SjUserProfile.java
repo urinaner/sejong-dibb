3e7901f45801883999f042af146dd606d7936206
장영재 <urinaner@naver.com>
2025-01-19 15:33:47 +0900

    feat: 유저 정보 dto

---
+++ backend/src/main/java/org/example/backend/users/dto/SjUserProfile.java
@@ -0,0 +1,19 @@
+package org.example.backend.users.dto;
+
+public class SjUserProfile {
+    private final String name;
+    private final String major;
+
+    public SjUserProfile(String name, String major) {
+        this.name = name;
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+}
