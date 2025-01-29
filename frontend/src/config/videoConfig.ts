8ef4fc2f7ec47aa124f7232002f31f402bf2cac4
pillow12360 <ehdcks1256@naver.com>
2025-01-29 21:00:39 +0900

    #313 refactor: cloudflare r2 uri 적용

--- frontend/src/config/videoConfig.ts
+++ frontend/src/config/videoConfig.ts
@@ -1,6 +1,6 @@
 import { Video } from '../components/Banner/VideoBanner/types';
 
-export const CDN_URL = process.env.NEXT_PUBLIC_CDN_URL || '';
+const CDN_URL = 'https://pub-e4ab6fbbf2154b7bb658f18011c1b707.r2.dev';
 
 export const mainVideos: Video[] = [
   {
@@ -12,39 +12,39 @@ export const mainVideos: Video[] = [
         quality: '1080p',
       },
       {
-        src: `${CDN_URL}/assets/videos/output_main_720p.mp4`,
+        src: `${CDN_URL}/output_main_720p.mp4`,
         type: 'video/mp4',
         quality: '720p',
       },
       {
-        src: `${CDN_URL}/assets/videos/output_main.webm`,
+        src: `${CDN_URL}/output_main.webm`,
         type: 'video/webm',
         quality: '1080p',
       },
     ],
-    poster: `${CDN_URL}/assets/videos/main_poster.jpg`,
+    poster: `${CDN_URL}/main_poster.jpg`,
     title: ['integrative', 'bioscience', 'biotechnology'],
   },
   {
     id: '2',
     sources: [
       {
-        src: `${CDN_URL}/assets/videos/output_main_2_1080p.mp4`,
+        src: `${CDN_URL}/output_main_2_1080p.mp4`,
         type: 'video/mp4',
         quality: '1080p',
       },
       {
-        src: `${CDN_URL}/assets/videos/output_main_2_720p.mp4`,
+        src: `${CDN_URL}/output_main_2_720p.mp4`,
         type: 'video/mp4',
         quality: '720p',
       },
       {
-        src: `${CDN_URL}/assets/videos/output_main_2.webm`,
+        src: `${CDN_URL}/output_main_2.webm`,
         type: 'video/webm',
         quality: '1080p',
       },
     ],
-    poster: `${CDN_URL}/assets/videos/main_2_poster.jpg`,
+    poster: `${CDN_URL}/main_2_poster.jpg`,
     title: ['integrative', 'bioscience', 'biotechnology'],
   },
 ];
