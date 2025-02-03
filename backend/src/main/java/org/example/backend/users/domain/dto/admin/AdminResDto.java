0169ea181dd3775420d93819354a2a5b4eef8a2a
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-03 14:48:43 +0900

    feat: 관리자 비밀번호 수정 기능 구현

--- backend/src/main/java/org/example/backend/users/domain/dto/admin/AdminResDto.java
+++ backend/src/main/java/org/example/backend/users/domain/dto/admin/AdminResDto.java
@@ -1,7 +1,9 @@
 package org.example.backend.users.domain.dto.admin;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.backend.users.domain.entity.Users;
 
 @Data
 @NoArgsConstructor
@@ -9,4 +11,17 @@ public class AdminResDto {
 
     private String loginId;
     private String email;
+
+    @Builder
+    private AdminResDto(String loginId, String email) {
+        this.loginId = loginId;
+        this.email = email;
+    }
+
+    public static AdminResDto of(Users admin) {
+        return AdminResDto.builder()
+            .loginId(admin.getLoginId())
+            .email(admin.getEmail())
+            .build();
+    }
 }
