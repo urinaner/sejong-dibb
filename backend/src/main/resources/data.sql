4408b78d7b406324416381bd45f320649997f412
Sumi Chung <drsoombre@naver.com>
2025-02-10 15:52:08 +0900

    refactor: member 정보를 저장하지 않으므로 더미 데이터 삭제

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -33,10 +33,7 @@ VALUES (1, '바이오융합공학과',
 -- users 더미 데이터
 INSERT INTO users (users_id, login_id, password, username, email, phoneN, role)
 VALUES (1, 'admin', '$2a$10$FKHTTHcEkAZZGW9XqGtPfOx.apKljbCLvYESM05YbLWzDynnacLPO', '관리자', 'admin@example.com',
-        '010-1234-5678', 'ROLE_ADMIN'),
-       (2, 'member', '$2a$10$FKHTTHcEkAZZGW9XqGtPfOx.apKljbCLvYESM05YbLWzDynnacLPO', '멤버', 'member@example.com',
-        '010-1111-2222', 'ROLE_MEMBER');
-
+        '010-1234-5678', 'ROLE_ADMIN');
 
 -- Professor 더미 데이터 (10개)
 INSERT INTO professor (professor_id, name, major, phone, email, position, homepage, lab, profile_image)
