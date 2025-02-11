f3c1844db7274aa3be6e3e77eb38654f4ceb08a1
2jin2.1031 <2jin2.1031@gmail.com>
2025-02-11 18:50:06 +0900

    refactor: 사용하지 않는 메서드 삭제

--- backend/src/main/java/org/example/backend/users/repository/UsersRepository.java
+++ backend/src/main/java/org/example/backend/users/repository/UsersRepository.java
@@ -5,11 +5,7 @@ import org.example.backend.users.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    Users findByUsername(String username);
-
     Optional<Users> findByLoginId(String loginId);
-
-    Boolean existsByUsername(String username);
 
     Boolean existsByLoginId(String loginId);
 }
