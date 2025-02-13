de844002fe9fd32a930f877e29d55fcfc2cf1ca0
장영재 <urinaner@naver.com>
2025-02-13 21:00:05 +0900

    refactor: 삭제 시 검증 로직 추가 & 예외처리 추가

--- backend/src/main/java/org/example/backend/reservation/exception/ReservationExceptionType.java
+++ backend/src/main/java/org/example/backend/reservation/exception/ReservationExceptionType.java
@@ -11,12 +11,12 @@ import org.springframework.http.HttpStatus;
 public enum ReservationExceptionType implements BaseExceptionType {
 
     NOT_FOUND_RESERVATION(NOT_FOUND, "예약을 찾을 수 없습니다"),
-    NOT_VALID_RESERVATION_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     EXIST_ALREADY_RESERVATION(BAD_REQUEST, "해당 시간에 이미 승인된 예약이 존재합니다."),
     CONFLICT_TIMETABLE(BAD_REQUEST, "해당 시간에 정기 수업이 있어 예약이 불가능합니다."),
-    INVALID_TIME_ORDER(BAD_REQUEST, "시작 시간은 종료 시간보다 이후일 수 없습니다."),
+    INVALID_TIME_ORDER(BAD_REQUEST, "시작시간과 종료시간이 유효하지 않습니다."),
     EXCEEDS_MAX_DURATION(BAD_REQUEST, "예약은 최대 2시간까지만 가능합니다."),
     PAST_TIME_RESERVATION(BAD_REQUEST, "과거의 시간으로 예약할 수 없습니다."),
+    FORBIDDEN_OPERATION(BAD_REQUEST, "다른 사용자의 예약입니다.")
     ;
 
     private final HttpStatus httpStatus;
--- backend/src/main/java/org/example/backend/users/exception/member/MemberExceptionType.java
+++ backend/src/main/java/org/example/backend/users/exception/member/MemberExceptionType.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum MemberExceptionType implements BaseExceptionType {
-    DEPARTMENT_NOT_BIO(HttpStatus.BAD_REQUEST, "바이오융합공학과 소속이 아닙니다.");
+    DEPARTMENT_NOT_BIO(HttpStatus.BAD_REQUEST, "바이오융합공학과 소속이 아닙니다."),
+    NOT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "사용자를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
