0fcf46a48f721a282875945f33cf931e96c14df4
Sumi Chung <drsoombre@naver.com>
2025-02-27 17:23:49 +0900

    fix: 테이블 이름 수정

--- backend/src/main/java/org/example/backend/log/domain/RequestResponseLog.java
+++ backend/src/main/java/org/example/backend/log/domain/RequestResponseLog.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "request_response_log")
 public class RequestResponseLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
