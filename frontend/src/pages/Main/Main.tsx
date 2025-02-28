3bd905a8ef7ccf2c2939a85078ed7b85e33ca9c8
김희원 <julian3306@naver.com>
2025-02-28 22:28:46 +0900

    #426 feat: 공지사항 하단 세미나 컴포넌트 주석 처리

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -14,7 +14,7 @@ function Main(): JSX.Element {
         <ContentWrapper>
           <AnnouncementAndSeminar>
             <AnnouncementSection />
-            <SeminarSection />
+            {/* <SeminarSection /> */}
           </AnnouncementAndSeminar>
           <ShortcutSection />
         </ContentWrapper>
