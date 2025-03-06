f347a9481df8d8073e05fbfce6bc4feea0884122
Sumi Chung <drsoombre@naver.com>
2025-03-06 13:20:20 +0900

    feat: 논문 및 세미나가 BaseEntity를 상속

--- backend/src/main/java/org/example/backend/seminar/domain/entity/Seminar.java
+++ backend/src/main/java/org/example/backend/seminar/domain/entity/Seminar.java
@@ -1,18 +1,23 @@
 package org.example.backend.seminar.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.backend.common.domain.BaseEntity;
 import org.example.backend.common.utils.TimeParsingUtils;
 import org.example.backend.seminar.domain.dto.SeminarReqDto;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Seminar {
+public class Seminar extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,7 +46,8 @@ public class Seminar {
     private String company;
 
     @Builder
-    private Seminar(String name, String writer, String place, LocalDateTime startTime, LocalDateTime endTime, String speaker, String company) {
+    private Seminar(String name, String writer, String place, LocalDateTime startTime, LocalDateTime endTime,
+                    String speaker, String company) {
         this.name = name;
         this.writer = writer;
         this.place = place;
