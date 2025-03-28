fe1002b7e82f3069294883690f80a87ce715990c
장영재 <urinaner@naver.com>
2025-03-28 15:55:53 +0900

    refactor: 메인 로직에서 어노테이션 분리

---
+++ backend/src/main/java/org/example/backend/global/config/async/AsyncConfig.java
@@ -0,0 +1,9 @@
+package org.example.backend.global.config.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
