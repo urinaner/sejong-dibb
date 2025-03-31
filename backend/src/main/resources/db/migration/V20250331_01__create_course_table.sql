0f255c5adbe076af9213fbd72d865b92f8485dd3
장영재 <urinaner@naver.com>
2025-03-31 16:29:54 +0900

    feat: flyway 교과과정 타입 조회 추가

--- backend/src/main/resources/db/migration/V20250331_01__create_course_table.sql
+++ backend/src/main/resources/db/migration/V20250331_01__create_course_table.sql
@@ -1,11 +1,19 @@
 CREATE TABLE courses (
                          course_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                         academic_year_semester VARCHAR(10) NOT NULL,
-                         classification VARCHAR(50),
+
+                         course_type ENUM('UNDERGRADUATE', 'GRADUATE') NOT NULL,
+
                          course_number VARCHAR(20) NOT NULL,
                          course_name VARCHAR(100) NOT NULL,
                          course_name_en VARCHAR(100),
                          credit_time VARCHAR(20),
+
+                         academic_year_semester VARCHAR(10),
+                         classification VARCHAR(50),
+                         course_description TEXT,
+                         course_description_en TEXT,
+
+                         target_program VARCHAR(50),
 
                          created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
                          updated_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
