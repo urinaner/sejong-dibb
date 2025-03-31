3002491ea774059be8fa08c5f6855307dc394df7
장영재 <urinaner@naver.com>
2025-03-31 15:47:55 +0900

    feat: 교과과목 service 구현

---
+++ backend/src/main/java/org/example/backend/course/service/CourseService.java
@@ -0,0 +1,57 @@
+package org.example.backend.course.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.backend.course.domain.dto.CourseReqDto;
+import org.example.backend.course.domain.dto.CourseResDto;
+import org.example.backend.course.domain.entity.Course;
+import org.example.backend.course.exception.CourseException;
+import org.example.backend.course.exception.CourseExceptionType;
+import org.example.backend.course.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Transactional
+    public Long saveCourse(CourseReqDto courseReqDto) {
+        Course course = Course.of(courseReqDto);
+        Course savedCourse = courseRepository.save(course);
+        return savedCourse.getId();
+    }
+
+    public CourseResDto getCourse(Long courseId) {
+        Course course = findCourseById(courseId);
+        return CourseResDto.of(course);
+    }
+
+    @Transactional
+    public CourseResDto updateCourse(Long courseId, CourseReqDto courseReqDto) {
+        Course course = findCourseById(courseId);
+        course.update(courseReqDto);
+        return CourseResDto.of(course);
+    }
+
+    @Transactional
+    public void deleteCourse(Long courseId) {
+        Course course = findCourseById(courseId);
+        courseRepository.delete(course);
+    }
+
+    private Course findCourseById(Long courseId) {
+        return courseRepository.findById(courseId)
+                .orElseThrow(() -> new CourseException(CourseExceptionType.NOT_FOUND_COURSE));
+    }
+
+    public List<CourseResDto> getAllCourses() {
+        return courseRepository.findAll()
+                .stream()
+                .map(CourseResDto::of)
+                .toList();
+    }
+}
