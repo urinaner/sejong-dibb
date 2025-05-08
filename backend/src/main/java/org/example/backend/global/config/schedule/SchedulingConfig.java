8f8d392f3cafe3c9a598cad58580b49bd47da46d
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-05-08 19:00:17 +0900

    chore: 스케줄러 활성화 설정을 Configuration 클래스로 추출

---
+++ backend/src/main/java/org/example/backend/global/config/schedule/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package org.example.backend.global.config.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
