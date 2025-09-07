cc00381789849bc66f5586ec05a8c1ceb2bef2e3
pillow12360 <ehdcks1256@naver.com>
2025-09-07 21:43:23 +0900

    #484 feat: msw 의존성 설치 및 세팅

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  if (process.env.REACT_APP_API_MOCKING !== 'enabled') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+  // 서비스 워커 시작 + 미핸들 요청 경고
+  return worker.start({ onUnhandledRequest: 'warn' });
+}
+
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement,
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
